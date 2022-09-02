3d6a9d76d8c13112f0de68d07c1c0acead778fa9
Benjamin. L. Whelan <whelbe@icloud.com>
2022-09-02 10:35:18 +0200

    clarification

--- blockchain-cli/src/blockchain.ts
+++ blockchain-cli/src/blockchain.ts
@@ -1,49 +1,49 @@
 import * as crypto from 'crypto';
 
-class Block {   // TODO: Use interface
-  readonly hash: string;
+class Block {
+    readonly hash: string;
 
-  constructor (
-    readonly index: number,
-    readonly previousHash: string,
-    readonly timestamp: number,
-    readonly data: string
-  ) {
-    this.hash = this.calculateHash();
-  }
+    constructor(
+        readonly index: number, // unique identifier (sequential)
+        readonly previousHash: string,  //  previous block's hash
+        readonly timestamp: number, // creation dateTime
+        readonly data: string // transaction
+    ) {
+        this.hash = this.calculateHash();   // on creation
+    }
 
-  private calculateHash(): string {
-    const data = this.index + this.previousHash + this.timestamp + this.data;
-    return crypto
-             .createHash('sha256')
-             .update(data)
-             .digest('hex');
-  }
+    private calculateHash(): string {
+        const data = this.index + this.previousHash + this.timestamp + this.data; // concatenate all variables in function together (except its own hash)
+        return crypto
+            .createHash('sha256') // instance (Hash object) for generator
+            .update(data) // hash variable inside of Hash object updated to the related data
+            .digest('hex'); // convert hash into hexadecimal string
+    }
 };
 
 class Blockchain {
-  private readonly chain: Block[] = [];
+    private readonly chain: Block[] = [];
 
-  private get latestBlock(): Block {
-    return this.chain[this.chain.length - 1];
-  }
+    private get latestBlock(): Block {
+        return this.chain[this.chain.length - 1];
+    }
 
-  constructor() {
-    // Create the genesis block.
-    this.chain.push(new Block(0, '0', Date.now(), 'Genesis block'));
-  }
+    constructor() {
+        // Create the genesis block.
+        this.chain.push(new Block(0, '0', Date.now(), 'Genesis block'));
+    }
 
-  addBlock(data: string): void {
+    addBlock(data: string): void {
 
-    const block = new Block(
-      this.latestBlock.index + 1,
-      this.latestBlock.hash,
-      Date.now(),
-      data
-    );
+        const block = new Block(
+            this.latestBlock.index + 1,
+            this.latestBlock.hash,
+            Date.now(),
+            data
+        );
 
-    this.chain.push(block);
-  }
+        this.chain.push(block);
+    }
 }
 
 console.log('Creating the blockchain with the genesis block...');
